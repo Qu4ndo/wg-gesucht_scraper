ccfb5cb49badf026c9ed6d2068251369c7403f56
Manuel <pfandlmanuel@protonmail.com>
2019-12-30 19:49:02 +0100

    Implement pushover

---
+++ push.py
@@ -0,0 +1,14 @@
+import requests
+
+#Change to your pushover token!
+user_token = 'USER-KEY'
+
+def send_push_message(app_key, title, message):
+	push_data = {
+		'token': app_key,
+		'user': user_token,
+		'title': title,
+		'message': message
+	}
+
+	post_request = requests.post('https://api.pushover.net/1/messages.json', data=push_data)
